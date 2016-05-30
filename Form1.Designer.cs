de369deec393560246dfe40577c7e8d975139d32
janoncho90 <jotacardenas90@gmail.com>
2016-05-30 19:27:04 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace JuegoPalabras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFrase = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.lblPizarra = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese una Frase";
+            // 
+            // txtFrase
+            // 
+            this.txtFrase.Location = new System.Drawing.Point(111, 10);
+            this.txtFrase.Name = "txtFrase";
+            this.txtFrase.Size = new System.Drawing.Size(369, 20);
+            this.txtFrase.TabIndex = 1;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(486, 8);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 2;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // lblPizarra
+            // 
+            this.lblPizarra.Location = new System.Drawing.Point(13, 46);
+            this.lblPizarra.Name = "lblPizarra";
+            this.lblPizarra.Size = new System.Drawing.Size(548, 321);
+            this.lblPizarra.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 376);
+            this.Controls.Add(this.lblPizarra);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtFrase);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "El juego de Palabras";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFrase;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label lblPizarra;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoPalabras
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+
+            List<string> palabras = new List<string>();//este es un arreglo dinamico
+
+            string frase = txtFrase.Text;
+            int cantidadPalabras = 0;
+
+            string temporal = "";
+            for(int i=0; i < frase.Length; i++)
+            {
+                char caracter = frase[i];
+                if (caracter == ' ')
+                {
+                    palabras.Add(temporal);
+                    temporal = "";
+                    cantidadPalabras++;
+                }
+                else
+                {
+                    temporal += caracter;
+                }
+            }
+            if (frase.Length > 0)
+            {
+                palabras.Add(temporal);
+                cantidadPalabras++;
+            }
+            
+            lblPizarra.Text = "Esta frase tiene " + cantidadPalabras + " palabras.\n";
+
+            for (int i = 0; i < palabras.Count; i++)
+            {
+                lblPizarra.Text += "Palabra " + (i + 1) + ": " + palabras[i] + " tiene " + palabras[i].Length + " letras\n";
+            }
+        }
+    }
+}
